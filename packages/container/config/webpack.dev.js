ef4d358d8c99cfce891a297d160584847bde58a0
Muhammad.Ahsan <Muhammad.Ahsan@BC1-DEV-MAHSAN>
2025-02-04 10:01:43 +0500

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJSON = require('.././package.json');
@@ -21,10 +20,7 @@ const devConfig = {
         },
         shared: packageJSON.dependencies,
         // shared: ['react', 'react-dom'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
+    })
   ],
 };
 
